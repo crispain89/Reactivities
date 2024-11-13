7c8e6dd95fe9f308af104c3c2b70b330d9d9a3f7
cristinaaran89 <cristinaaran89@gmail.com>
2024-11-13 13:27:59 +0100

    finalizando la 2 parte

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapControllers();
+// vamos a poner la inyecccion de dependencias para el servicio de crear y actualizar la base de datos cada vez que arranquemos la app.
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+// creación de la base de Datos
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "ha ocurrido un problema durante la migración");
+}
+app.Run();
